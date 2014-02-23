d14564bde03097941c7d3d4930630e844205b6de
M@ <mattdunlap+github@gmail.com>
2014-02-23 18:10:15 -0500

    ready to start testing closed loop control

--- testArm/closed_loop_test.py
+++ testArm/closed_loop_test.py
@@ -4,15 +4,21 @@
 
 from pid import PIDController
 from stop import stopEverything
+import time
 
 def readInt(filename):
   with open(filename) as f:
     return int(f.read())
 
 PWM_PERIOD = readInt("/sys/devices/ocp.3/pwm_test_P9_22.13/period")
+SENSOR_RANGE = (1420, 3570)  # these values came from a run of calibrate.py
 
 def readAin():
-  return readInt("/sys/bus/iio/devices/iio:device0/in_voltage3_raw")
+  raw = readInt("/sys/bus/iio/devices/iio:device0/in_voltage3_raw")
+  return getPercentageIntoRange(SENSOR_RANGE, raw)
+
+def getPercentageIntoRange(range, distance):
+  return (distance - range[0]) / (range[1] - range[0])
 
 def writePwm(name, val):
   with open("/sys/devices/ocp.3/pwm_test_%s/duty" % name, mode="w+") as f:
@@ -32,14 +38,16 @@ def executePwmPair(pair):
   writePwm("P9_21.12", pair[1])
 
 def main():
-  sensorRange = (1420, 3570)  # these values came from a run of calibrate.py
-  pwmRange = (PWM_PERIOD/5, PWM_PERIOD)
-
-
-
-
-
-
+  pid = PIDController(1., 0, .1)
+  lastTime = time.time()
+  while True:
+    reading = readAin()
+    now = time.time()
+    control = pid.update(.5, reading, now - lastTime)
+    pair = mapControlToPwmPair(control)
+    executePwmPair(pair)
+    lastTime = now
+    time.sleep(.1)  # because the valve response rate is 10Hz
 
 
 
