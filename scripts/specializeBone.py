936c701201e51eeed32c82dbf8e7323007ad3a3b
M@ <mattdunlap+github@gmail.com>
2015-11-08 16:15:19 -0500

    stubbed in specializeBone

---
+++ scripts/specializeBone.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Beaglebone Specialization Script
+
+Configures a flashed Beaglebone to be one of the six legs, three hips, or other computers.
+
+Usage:
+  ./specializeBone.py [options]
+
+Options:
+  -h, --help            Show this help screen.
+  --version             Show the version.
+  --leg=<leg>           Indicates which leg this BBB will be installed into (CW when viewed from above).
+  --hip=<hip>           Indicates which hips this BBB will control (1=front, 3=rear).
+  --console             Indicates that this BBB is the new console computer.
+"""
+
+def changeHostname(name):
+  pass
+
+def changeIpAddress(ip):
+  pass
+
+def disableDhcpd():
+  pass
+
+def disableNtpd():
+  pass
+
+if __name__ == '__main__':
+  from docopt import docopt
+  args = docopt(__doc__, version="Pin Configurationist v0.1")
