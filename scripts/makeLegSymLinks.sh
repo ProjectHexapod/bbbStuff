d2c96f9b054adb783f914f155f058aa6b7c065db
M@ <mattdunlap+github@gmail.com>
2016-02-06 10:00:37 -0500

    fixed typo

--- scripts/makeLegSymLinks.sh
+++ scripts/makeLegSymLinks.sh
@@ -13,7 +13,7 @@ ln -s /sys/class/pwm/pwmchip0/pwm1/duty_cycle /mnt/hardware/pwm_thigh_up
 # Analog inputs
 ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage4_raw /mnt/hardware/ain_knee_str_pot   
 ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage6_raw /mnt/hardware/ain_thigh_str_pot  
-ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage5_raw /mnt/hardware/ain_complient_link 
+ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage5_raw /mnt/hardware/ain_compliant_link 
 ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage2_raw /mnt/hardware/ain_thigh_feedback 
 ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage3_raw /mnt/hardware/ain_knee_feedback  
 ln -s /sys/devices/platform/ocp/44e0d000.tscadc/TI-am335x-adc/iio:device0/in_voltage0_raw /mnt/hardware/ain_battery_voltage
