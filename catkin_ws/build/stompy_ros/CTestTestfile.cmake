f0b54c0dfa68be935faa262c109e1fe53411bfbb
ChrisStokes <stokes0520@gmail.comgit config --global user.name ChrisStokesgit config --global user.email stokes0520@gmail.com>
2016-04-03 16:48:53 -0400

    talker node added

---
+++ catkin_ws/build/stompy_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/stompy/my_bbbStuff/catkin_ws/src/stompy_ros
+# Build directory: /home/stompy/my_bbbStuff/catkin_ws/build/stompy_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
