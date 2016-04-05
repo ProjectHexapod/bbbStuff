21732f964728745866d3ccf573a0bb0cf4574c16
becomesaflame <spark.scul@gmail.com>
2016-04-05 03:02:01 +0000

    removed unneeded catkin workspace files

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stompy/my_bbbStuff/catkin_ws/src
-# Build directory: /home/stompy/my_bbbStuff/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(stompy_ros)
--- catkin_ws/build/stompy_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stompy/my_bbbStuff/catkin_ws/src/stompy_ros
-# Build directory: /home/stompy/my_bbbStuff/catkin_ws/build/stompy_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
